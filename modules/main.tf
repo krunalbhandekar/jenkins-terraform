e987b8968b0eb60633782cabf90554005a735ccc
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-06-25 10:31:11 +0530

    fix: Fixed security group issue

--- modules/main.tf
+++ modules/main.tf
@@ -16,11 +16,11 @@ resource "aws_instance" "terraform_master" {
 }
 
 resource "aws_instance" "terraform_agent" {
-  count           = var.agent_count
-  ami             = data.aws_ami.machine.id
-  key_name        = aws_key_pair.jenkins_key.key_name
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.jenkins_sg.id]
+  count                  = var.agent_count
+  ami                    = data.aws_ami.machine.id
+  key_name               = aws_key_pair.jenkins_key.key_name
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
   user_data = file("${path.module}/scripts/${var.os_type}/jenkins_agent.sh")
 
